ae2d722068b970cf794b29b2f611de2daff303ac
HollenbeckChad <chad.hollenbeck@owlcomputing.com>
2016-03-23 17:12:13 -0400

    Added comments to php script functionality for ease of creation.

--- scripts/js/db.service.js
+++ scripts/js/db.service.js
@@ -1,12 +1,16 @@
 ﻿///Holds all the Ajax calls for storing information back to the database. 
+///Always return raw HTML that will be loaded into the content div on the index.html
+///All PHP scripts are used as navigation between the webpages.
 
+//Load links to all the mad libs
+//Each link will call the php script getMadLibById.php
 function getAllMadLibs ()
 {
     $.ajax({
         type: "GET",
         url: 'getMadLibs.php',
         success: function (data) {
-            $('#container').html(data);
+            $('#content').html(data);
         },
         error: function (data) {
             $.Jgrowl("An Error occured loading mad libs.");
@@ -14,13 +18,16 @@ function getAllMadLibs ()
     });
 }
 
+//Return the HTML for filling out the mad lib
+//Includes a form with labels, fields, and a submit button
+//submit button can use javascript to display the completed MadLib
 function getMadlibById(id)
 {
     $.ajax({
         type: "GET",
         url: 'getMadLibById.php',
         success: function (data) {
-            $('#container').html(data);
+            $('#content').html(data);
         },
         error: function(data)
         {
@@ -29,6 +36,24 @@ function getMadlibById(id)
     });
 }
 
+//Return the HTML for displaying the homepage text. 
+//Include a bit about our group and what the website will do
+function getHome() {
+    $.ajax({
+        type: "GET",
+        url: 'getHome.php',
+        success: function (data) {
+            $('#content').html(data);
+        },
+        error: function (data) {
+            $.jGrowl('An error occured loading the home page');
+        }
+    });
+}
+
+
+//Send a stringified JSON object back to the php file. 
+//JSON object should be parsed on reciept and saved to the database.
 function saveMadLib(madLib)
 {
     $.ajax({
