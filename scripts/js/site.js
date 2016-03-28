6c184e760a28164d46bedaae02da606543641ba0
Gage Vander Clay <vandercg@arch08.cis.gvsu.edu>
2016-03-28 17:25:59 -0400

    make a function that creates a homeopage

--- scripts/js/site.js
+++ scripts/js/site.js
@@ -1,2 +1,11 @@
 function constructHome(){
+    
+    $("#content").html(
+    "<h3 class=\"descriptionHeader\">A website to make and play Mad Libs</h3><br><br>" +
+    "<button type=\"button\" class=\"btn btn-primary btn-lg btn-block\">Do a Mad Lib</button>" +
+    "<button type=\"button\" class=\"btn btn-primary btn-lg btn-block\">Make a Mad Lib</button>" );
+    
 }
+
+
+$(document).ready(constructHome);
