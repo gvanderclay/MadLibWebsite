a0ae47a10f8c7c130b6f40d4fd3b04ed46d084d9
Trey Jardine <jardinet@arch07.cis.gvsu.edu>
2016-03-30 17:20:00 -0400

    Edited php

--- scripts/php/getMadLibById.php
+++ scripts/php/getMadLibById.php
@@ -1,19 +1,52 @@
+<html>
+<head></head>
+<body>
+
+fucking hi
 <?php 
-session_start();
+
 //get madlib by ID
 
-$cmdText = 'SELECT TITLE, MADLIB_TEXT, RATING FROM MadLibs
-WHERE SEQ_ID = ' . $_POST['ID'];
+
+$servername = "cis.gvsu.edu";
+$username = "jardine";
+$password = "jardine1214";
+$dbname = "jardine";
+
+// Create connection
+        $conn = mysql_connect($servername, $username, $password)
+                or die("Could not connect..." . mysql_error($conn) );
+
+        $selected = mysql_select_db("jardine",$conn)
+  or die("Could not select examples" . mysql_error($conn));
 
 
-$result = mysql_query($cmdText);
+
+
+$cmdText = 'SELECT TITLE, MADLIB_TEXT, RATING FROM MadLibs WHERE SEQ_ID = 3';
+
+
+$result = mysql_query($cmdText, $conn);
 if (!$result) {
     die('Invalid query: ' . mysql_error());
 }
 
-$decode = json_decode($result, true);
 
 
-echo $decode;
+//echo 'Hello' . mysql_fetch_row($result);
+
+echo 'new' . $result
+
+//$decode = json_decode($result, true);
+
+
+//echo $decode;
+
+mysql_close($conn);
+
 
 ?>
+
+</body>
+
+</html>
