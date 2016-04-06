4277cc224d140f1667a6d30a656f2d6c1bce41ce
gvanderclay <2fat4doors>
2016-04-06 13:58:38 -0400

    Dynamically load categories and MadLibs and put them into the html

--- scripts/js/script.js
+++ scripts/js/script.js
@@ -1,9 +1,8 @@
 var processMadLibsJson = function(madLibs){
-    var request = $.ajax({
+    $.ajax({
         url: "resources/choosemadlib.html",
         success: function(data){
-           $("#content").html(data);
-
+            makeMadLibList(data, madLibs);
         },
         error: function(data){
             console.log(data);
@@ -11,23 +10,54 @@ var processMadLibsJson = function(madLibs){
     });
 }
 
-var makeMadLibList = function(madLibs){
+var makeMadLibList = function(html, madLibs){
     var madLibsJSON = jQuery.parseJSON(madLibs);
+    html = '<div>' + html + '</div>';
+    var jHtml = $(html);
+    if(madLibsJSON.length == 0){
+        showNoMadLibs();
+        return;
+    }
     for(var madLibIndex in madLibsJSON){
         var madLib = madLibsJSON[madLibIndex];
-        $().append("<button type=\"button\" class=\"list-group-item\">" + madLib[0] + "</button>");
-        console.log($("#madLibList").length);
+        jHtml.find("div#madLibList").append($('<button type="button" class="list-group-item">' + madLib +'</button>')).html();
     }
+    $("#content").html(jHtml);
+}
+
+var showNoMadLibs = function(){
+    $.ajax({
+        url: "resources/noMadLibs.html",
+        success: function(data){
+            $("#content").html(data);
+        },
+        error: function(data){
+            console.log(data);
+        }
+    });
 }
 
 var loadCreatePage = function(){
      $.ajax({
         url: "resources/madlibcreation.html",
         success: function(data){
-           $("#content").html(data); 
+            loadCategories(data);
         },
         error: function(data){
             console.log(data);
         }
     });
+}
+
+var categories = ["College", "Food", "Politics", "Pop Culture", "Sports", "Outdoors"];
+
+var loadCategories = function(html){
+    html = '<div>' + html + '</div>';
+    var jHtml = $(html);
+    for(var index in categories){
+        var category = categories[index];
+        jHtml.find('select#category').append(' <option>' + category + '</option>').html();
+       // console.log(jHtml.html());
+    }
+    $("#content").html(jHtml);
 }
