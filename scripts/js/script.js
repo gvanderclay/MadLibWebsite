774391794255f0d0a03996de5bca7cd1da23f9ef
gvanderclay <2fat4doors>
2016-04-11 14:32:16 -0400

    Mad lib is now processed by the web page

--- scripts/js/script.js
+++ scripts/js/script.js
@@ -14,7 +14,7 @@ var processMadLibsJson = function(madLibs){
         },
         complete: function () { $('#loading').hide(); }
     });
-}
+};
 
 /**
  * Makes the list of mad libs and puts them into the html
@@ -31,8 +31,6 @@ var makeMadLibList = function(html, madLibs){
         showNoMadLibs();
         return;
     }
-
-    var buttonHTML = '<div class="btn-group-vertical">';
     // iterate through the madLibs and add the buttons
     for(var madLibIndex in madLibsJSON){
         var madLib = madLibsJSON[madLibIndex];
@@ -46,41 +44,18 @@ var makeMadLibList = function(html, madLibs){
             var pattern = /madLibBtn(\d*)/g;
             var id = pattern.exec(this.id)[1];
             getMadlibById(id);
-        })
-        // buttonHTML +=
-        //     '<div class="btn-group madlib-list" role="group"><button id="btnGroupVerticalDrop' + madLibSplit[0] +
-        //     '" type="button" class="btn btn-secondary dropdown-toggle madlib" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">' +
-        //     madLibSplit[1] +
-        //     '</button><div class="dropdown-menu" aria-labelledby="btnGroupVerticalDrop' + madLibSplit[0] +
-        //     '"><a class="dropdown-item" href="#">Edit</a><br /><a class="dropdown-item" href="#">Fill Out</a></div></div>';
-
+        });
     }
-    // $("#content").html(jHtml);
-    //buttonHTML += '</div>';
 
     $('#content').html(jHtml);
-}
+};
 
 /**
  * This function is only called if no mad libs exist in the database
  */
 var showNoMadLibs = function(){
-    //$.ajax({
-    //    url: "resources/noMadLibs.html",
-    //    beforeSend: function () {
-    //        $('#loading').show();
-    //    },
-    //    success: function(data){
-    //        $("#content").html(data);
-    //    },
-    //    error: function(data){
-    //        console.log(data);
-    //    },
-    //    complete: function () { $('#loading').hide(); }
-    //});
-
     $('#content').load('resources/noMadLibs.html');
-}
+};
 
 /**
  * Load the page that is seen when creating a mad lib
@@ -99,7 +74,7 @@ var loadCreatePage = function(){
         },
         complete: function () { $('#loading').hide(); }
      });
-}
+};
 
 /* Array of categories that can be selected for a mad lib */
 var categories = ["College", "Misc", "Political", "Pop-Culture", "Sports", "Outdoors"];
@@ -114,11 +89,14 @@ var loadCategories = function(html){
     for(var index in categories){
         var category = categories[index];
         jHtml.find('select#category').append(' <option>' + category + '</option>').html();
-       // console.log(jHtml.html());
     }
     $("#content").html(jHtml);
-}
+};
 
+/**
+ * Start filling in fields for the mad lib
+ * @param madLib An incomplete mad Lib that can be done
+ */
 var doMadLib = function(madLib){
     $.ajax({
         url: "resources/doMadLib.html",
@@ -128,35 +106,116 @@ var doMadLib = function(madLib){
         success: function(data){
             var madLibJSON = jQuery.parseJSON(madLib);
             var descriptors = parseMadLib(madLibJSON[1]);
-            addMadLibDescriptors(data, descriptors);
+            addMadLibDescriptors(data, descriptors, madLibJSON[1]);
         },
         error: function(data){
             console.log(data);
         },
         complete: function () { $('#loading').hide(); }
     });
-}
+};
 
-var addMadLibDescriptors = function(html, descriptors){
+/**
+ * Add the descriptorTags to the web page
+ * @param html The preloaded html
+ * @param descriptorTags descriptors that are being added
+ * @param madLib The incomplete madLib
+ */
+var addMadLibDescriptors = function(html, descriptorTags, madLib){
     html = '<div>' + html + '</div>';
     var jHtml = $(html);
-    for(var index in descriptors){
-        var pattern = /\[(\w*)\]/g
-        var descriptor = descriptors[index];
-        descriptor = pattern.exec(descriptor)[1];
-        var input = '<div class ="form-group"><label for="descriptor' + index + '">' + descriptor + '</label><input type="text" class="form-control" id="descriptor' + index + '"></div>';
+    // insert each descriptor with a text area into the web page
+    for(var index in descriptorTags){
+        // remove the square brackets from the descriptorTags
+        var pattern = /\[(\w*)\]/g;
+        var descriptorTag = descriptorTags[index];
+        descriptorTag = pattern.exec(descriptorTag)[1];
+        // add text area and label
+        var input = '<div class ="form-group"><label for="descriptor' + index + '">' + descriptorTag + '</label><input type="text" class="form-control descriptorInput" id="descriptor' + index + '" required></div>';
         jHtml.find("form#madLibInput").append($(input)).html();
     }
-    jHtml.find("form#madLibInput").append($('<button type="button" class="btn btn-default" onclick="test()">Submit</button>')).html();
+    // add submit button
+    jHtml.find("form#madLibInput").append($('<button type="button" id="submitMadLib" class="btn btn-default">Submit</button>')).html();
+    // add functionality to submit button
+    jHtml.on('click', '#submitMadLib', function(){
+        processMadLibFormInput(madLib);
+    });
     $("#content").html(jHtml);
-}
+};
 
-var processMadLibFormInput = function(){
-    
-    alert("alert");
-}
+/**
+ * Processes the form input and puts it into the madLib
+ * @param madLib
+ */
+var processMadLibFormInput = function(madLib){
+    var descriptorTags = [];
+    var descriptors = [];
+    // get the tags and the input
+    $('div.form-group').each(function(){
+        descriptorTags.push($(this).find("label").text());
+        descriptors.push($(this).find("input").val());
+    });
+    // check if all fields are filled in and display the complete madLib if they are
+    if(validateInput()){
+        displayCompleteMadLib(madLib, descriptors, descriptorTags);
+    }
+};
 
-var testStr = "This is a simple test for my Mad Lib website. [Adjective] ad;fajf;l;akjijcicjijeinamn,nsdf,mn [Noun] a;dlkjbzoeowijrlknakjdf [a game]. a;sdjlzbhoiehoj [that game again].";
+/**
+ * Makes sure all required fields in form are filled out
+ * @returns {boolean} Whether or not all fields are filled
+ */
+var validateInput = function(){
+    // used to count blank fields
+    var blankInputs = 0;
+    // for each required field ...
+    $('[required]').each(function(){
+        // if the value is empty apply a border and count a blank input
+        if($(this).val().trim() == ""){
+            $(this).css('border', '1px solid red');
+            blankInputs++;
+        }
+        else{
+            // if not empty then reset the border
+            $(this).css('border-color', '');
+        }
+    });
+    var error = blankInputs > 0;
+    // display error message if there are blank inputs
+    if(error){
+        $.jGrowl("Please complete all required fields");
+    }
+    return !error;
+};
+
+/**
+ * Show the completed mad lib on the website
+ * @param madLib incomplete madlib
+ * @param descriptors descriptors to be added
+ * @param descriptorTags tags of the descriptors
+ * TODO complete this
+ */
+var displayCompleteMadLib = function(madLib, descriptors, descriptorTags){
+    var completedMadLib = insertWordsInMadLib(madLib, descriptors, descriptorTags);
+    console.log(completedMadLib);
+};
+
+/**
+ * Inserts the descriptors into the mad lib
+ * @param madLib incomplete mad lib
+ * @param descriptors descriptors to be put in
+ * @param descriptorTags tags of the descriptors
+ * @returns {*}
+ */
+var insertWordsInMadLib = function(madLib, descriptors, descriptorTags){
+    // for each descriptor replace the descriptor in the mad lib.
+    for(var index in descriptors){
+        var descriptor = descriptors[index];
+        var tag = descriptorTags[index];
+        madLib.replace('[' + tag + ']', descriptor);
+    }
+    return madLib;
+};
 
 /**
  * Gets the description words from the mad lib
@@ -166,6 +225,4 @@ var testStr = "This is a simple test for my Mad Lib website. [Adjective] ad;fajf
 var parseMadLib = function(str){
     var regex = /\[(.*?)\]/gm;
     return  str.match(regex);
-}
-
-$(document).ready(parseMadLib(testStr));
+};
